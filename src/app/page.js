da8786f134985496d179e28ef421606123858d9a
Ali <raza8r@gmail.com>
2023-07-26 02:04:16 +0500

    add navbar component

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,9 @@
+import Navbar from "@/components/Navbar";
+
 export default function Home() {
   return (
     <main className="min-h-screen">
-      <p>Food Sply</p>
+      <Navbar />
     </main>
   );
 }

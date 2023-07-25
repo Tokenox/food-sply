64b0aafc53e3af9b3c4e9b37f3f4f4bf4182e5d3
Ali <raza8r@gmail.com>
2023-07-26 02:48:41 +0500

    add navbar items

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,19 @@
+import { NavbarItems } from "@/utils/constants";
+import Image from "next/image";
 import React from "react";
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="section-padding-x flex justify-between items-center">
+      <Image src={"/logo.svg"} alt="Food Sply" width={151} height={54} />
+      <div className="flex gap-8 h-[54px] items-center">
+        {NavbarItems?.map((item) => (
+          <p className="navbar-hover-effect" key={item.title}>
+            {item.title}
+          </p>
+        ))}
+      </div>
+      <button>Login</button>
     </div>
   );
 };
---
+++ src/utils/constants.js
@@ -0,0 +1,26 @@
+export const NavbarItems = [
+  {
+    title: "Home",
+    url: "#home",
+  },
+  {
+    title: "Tokenomics",
+    url: "#tokenomics",
+  },
+  {
+    title: "Whitepaper",
+    url: "#whitepaper",
+  },
+  {
+    title: "NFT",
+    url: "#nft",
+  },
+  {
+    title: "Team",
+    url: "#team",
+  },
+  {
+    title: "Roadmap",
+    url: "#roadmap",
+  },
+];

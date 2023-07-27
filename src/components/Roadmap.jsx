91423ebcc03f45863baefb877dc94d5db43b224e
Ali <raza8r@gmail.com>
2023-07-28 03:04:12 +0500

    designed roadmap section

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -13,23 +13,60 @@ const Roadmap = () => {
           organization towards success.
         </p>
       </div>
-      <div className="">
-        <div className="">
-          <CardLeft
-            title={"Prototype Completion"}
-            slug={"prototype"}
-            description={`Crafting a comprehensive prototype within a span of <b> 4-6 months </b> to visualize our offering.`}
-          />
-          <CardLeft
-            title={"Food Accelerator Development"}
-            slug={"accelerator"}
-            description={`Committing <span> 4-8 months </span> to develop a Food Accelerator for rapid advancement in the field.`}
-          />
-          <CardLeft
-            title={"Financial Product Development"}
-            slug={"financial"}
-            description={`Dedicating <b> 6 months </b> to devise innovative financial products to support our stakeholders.`}
-          />
+      {/* //! Roadmap */}
+      <div className="flex justify-center">
+        <div className="flex gap-14 2xl:gap-[72px]">
+          {/* Left side */}
+          <div className="">
+            <CardLeft
+              title={"Prototype Completion"}
+              slug={"prototype"}
+              description={`Crafting a comprehensive prototype within a span of <b> 4-6 months </b> to visualize our offering.`}
+            />
+            <CardLeft
+              title={"Food Accelerator Development"}
+              slug={"accelerator"}
+              description={`Committing <span> 4-8 months </span> to develop a Food Accelerator for rapid advancement in the field.`}
+            />
+            <CardLeft
+              title={"Financial Product Development"}
+              slug={"financial"}
+              description={`Dedicating <b> 6 months </b> to devise innovative financial products to support our stakeholders.`}
+            />
+          </div>
+          {/* Center circles */}
+          <div className="bg-[#E6E6E6] w-1.5 relative h-[875px] mt-4">
+            <div className="roadmap-circle-1">
+              <Circle color="#DE54FF" />
+            </div>
+            <div className="roadmap-circle-2">
+              <Circle color="#B6D329" />
+            </div>
+            <div className="roadmap-circle-3">
+              <Circle color="#F9B545" />
+            </div>
+            <div className="roadmap-circle-4">
+              <Circle color="#5EB77A" />
+            </div>
+            <div className="roadmap-circle-5">
+              <Circle color="#DE6790" />
+            </div>
+          </div>
+          {/* Right side */}
+          <div className="">
+            <CardRight
+              title={"Agriculture Supply Chain Building"}
+              slug={"supply"}
+              description={`Initiating the construction of a decentralized supply chain for agriculture in the next <b> 3-6 months. </b>`}
+              className={"mt-[200px]"}
+            />
+            <CardRight
+              title={"Community-Based Agriculture Program Initiation"}
+              slug={"initiation"}
+              description={`Fostering local engagement by starting a community-based agriculture program.`}
+              className={"mt-[105px]"}
+            />
+          </div>
         </div>
       </div>
     </div>
@@ -45,11 +82,11 @@ const CardLeft = ({ title, description, slug }) => {
         className={classNames("bg-no-repeat w-[460px] h-[110px] relative", {
           ['bg-[url("/svg/roadmap/prototype.svg")]']: slug === "prototype",
           ['bg-[url("/svg/roadmap/accelerator.svg")]']: slug === "accelerator",
-            ['bg-[url("/svg/roadmap/financial.svg")]']: slug === "financial",
+          ['bg-[url("/svg/roadmap/financial.svg")]']: slug === "financial",
         })}
       >
         <p className="text-2xl font-bold capitalize text-white text-center pt-5">{title}</p>
-        <div className="bg-[#F2F2F2] w-[380px] h-[236px] roadmap-box flex justify-center items-center">
+        <div className="roadmap-box-left bg-[#F2F2F2] w-[380px] h-[236px] flex justify-center items-center">
           <p
             dangerouslySetInnerHTML={{ __html: description }}
             className={classNames(
@@ -62,12 +99,28 @@ const CardLeft = ({ title, description, slug }) => {
   );
 };
 
-const CardRight = ({ title, description }) => {
+const CardRight = ({ title, description, slug, className }) => {
   return (
-    <div>
-      <div className="bg-[url('/svg/roadmap/supply.svg')] bg-no-repeat w-[460px] h-[110px] relative">
-        <p className="text-2xl font-bold capitalize text-white text-center pt-5">{title}</p>
-        <div className="bg-[#F2F2F2] w-[380px] h-[236px] roadmap-box flex justify-center items-center">
+    <div
+      className={classNames("h-[312px]", {
+        [className]: className,
+      })}
+    >
+      <div
+        className={classNames("bg-no-repeat w-[460px] h-[110px] relative", {
+          ["bg-[url('/svg/roadmap/supply.svg')]"]: slug === "supply",
+          ['bg-[url("/svg/roadmap/initiation.svg")]']: slug === "initiation",
+        })}
+      >
+        <p
+          className={classNames("font-bold capitalize text-white text-center pt-5", {
+            ["text-2xl"]: slug === "supply",
+            ["text-xl h-[52px] pt-6"]: slug === "initiation",
+          })}
+        >
+          {title}
+        </p>
+        <div className="roadmap-box-right bg-[#F2F2F2] w-[380px] h-[236px] flex justify-center items-center">
           <p
             dangerouslySetInnerHTML={{ __html: description }}
             className={classNames(
@@ -80,9 +133,12 @@ const CardRight = ({ title, description }) => {
   );
 };
 
-const Circle = () => {
+const Circle = ({ color }) => {
   return (
-    <div className="w-[50px] h-[50px] rounded-full bg-orange">
+    <div
+      className={classNames("w-[50px] h-[50px] rounded-full flex justify-center items-center")}
+      style={{ backgroundColor: color }}
+    >
       <div className="w-[32px] h-[32px] rounded-full bg-white" />
     </div>
   );

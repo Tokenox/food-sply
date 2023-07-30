70a2889bc4d1547a40d2030f8c62a6bea25c6582
Ali <raza8r@gmail.com>
2023-07-31 04:27:54 +0500

    add values in to first bar

--- src/components/Tokenomics.jsx
+++ src/components/Tokenomics.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { AirdropIcon } from "../../public";
 
 const Tokenomics = () => {
   return (
@@ -10,13 +11,26 @@ const Tokenomics = () => {
           organization towards success.{" "}
         </p>
         <div className="mt-[100px] w-[140px]">
-          <div className="dark-color h-[85px] relative ">
+          <div className="dark-color h-[85px] relative">
+            <div className="flex justify-center items-center h-full">
+              <p className="airdrop-text -rotate-90 text-4xl font-bold pr-5">1%</p>
+            </div>
             <div className="dark-color  w-full h-7 rounded-[50%] absolute top-[-15px] border border-[#899AA1]" />
           </div>
-          <div className="liquidity-center w-full h-[210px] relative">
-            <div className="ellipse-middle w-full h-[32px] absolute top-[-15px]"></div>
-            <div className="airdrop-bottom-ellipse liquidity-center w-full h-[32px] absolute bottom-[-13px] z-[1]"></div>
-            <div className="airdrop-light w-full h-[31px] absolute bottom-[-15%]"></div>
+          <div className="airdrop-center w-full h-[210px] relative">
+            <div className="flex justify-between items-end h-full pb-7 px-2">
+              <div className="flex justify-between w-full items-center">
+                <p className="writing-mode font-xl font-bold text-white">Airdrop</p>
+                <AirdropIcon />
+              </div>
+            </div>
+            <div className="ellipse-middle w-full h-[32px] absolute top-[-15px]" />
+            <div className="airdrop-bottom-ellipse airdrop-center w-full h-[32px] absolute bottom-[-13px] z-[1]" />
+            <div className="airdrop-light w-full h-[31px] absolute bottom-[-15%]" />
+          </div>
+          <div className="mt-12 text-white text-xl text-center">
+            <p>Airdrop 1%</p>
+            <p>50,000,000,000$</p>
           </div>
         </div>
       </div>

d70f41b136753ce6c1f2bc18ccf19f6e747d920b
Ali <raza8r@gmail.com>
2023-08-04 23:58:27 +0500

    add price badge

--- src/common/Product.jsx
+++ src/common/Product.jsx
@@ -5,8 +5,11 @@ import Button from "./Button";
 const Product = ({ title, price, description, image, link }) => {
   return (
     <div className="w-[275px] product-card flex flex-col items-center relative">
-      <div className="bg-[url('/svg/product/round.svg')] w-[220px] h-[220px] flex flex-col items-center justify-center">
+      <div className="bg-[url('/svg/product/round.svg')] w-[220px] h-[220px] flex flex-col items-center justify-center relative">
         <Image src={image} alt={title} width={175} height={175} />
+        <div className="bg-orange w-14 h-14 rounded-full border-4 border-white absolute bottom-4 right-8 flex justify-center items-center">
+          {price}
+        </div>
       </div>
       <div className="product-card h-[200px] text-center px-7">
         <h4 className="text-light-green text-2xl capitalize my-2.5">{title}</h4>

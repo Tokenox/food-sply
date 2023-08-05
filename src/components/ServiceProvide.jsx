81db906dfe363da2cd2e9533bdf91d2efeaad04a
Ali <raza8r@gmail.com>
2023-08-05 06:03:03 +0500

    design service provide section for small screen

--- src/components/ServiceProvide.jsx
+++ src/components/ServiceProvide.jsx
@@ -4,17 +4,17 @@ import React from "react";
 
 const ServiceProvide = () => {
   return (
-    <div className="hidden lg:block section-margin lg:flex justify-between items-center gap-4">
+    <div className="section-margin flex flex-col lg:flex-row justify-between items-center gap-4">
       {/* First col */}
-      <div className="w-[550px]">
-        <h1 className="uppercase text-green text-5xl font-bold mb-7">Services we provide</h1>
-        <p className="text-xl leading-[150%] ">
+      <div className="w-full lg:w-[550px]">
+        <h1 className="uppercase text-green text-3xl text-center lg:text-left lg:text-5xl font-bold mb-5 lg:mb-7">Services we provide</h1>
+        <p className="text-base text-center lg:text-left lg:text-xl leading-[150%] text-text-dark">
           Agriculture is one of the largest industries in the world. Currently its going threw a
           revolution, let us help you navigate the new world.{" "}
         </p>
       </div>
       {/* Second col */}
-      <div className="w-[50%] flex justify-end">
+      <div className="w-[90%] mt-9 lg:mt-0 lg:w-[50%] flex justify-center lg:justify-end">
         <Image
           src={"/svg/service-provide.svg"}
           alt="Food sply services provides"

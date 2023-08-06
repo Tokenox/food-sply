3458c1940a89f9f48b664bd9df1308e361e25fc2
abubakarafzal444 <abubakarafzal444@gmail.com>
2023-08-06 17:04:37 +0500

    animation time fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -46,14 +46,19 @@ const Header = () => {
               <Fade duration={2500} triggerOnce>
                 <div>
                   <div className="mb-5">
-                    <Image src={"/svg/new-era.svg"} alt="New Era" width={344} height={30} />
+                    <Image
+                      src={"/svg/new-era.svg"}
+                      alt="New Era"
+                      width={344}
+                      height={30}
+                    />
                   </div>
                   <div>
                     <h1>
                       <Fade
                         delay={1e3}
                         cascade
-                        damping={1e-1}
+                        damping={0.04}
                         triggerOnce
                         className="text-border text-white font-bold text-[52px] capitalize mt-4 leading-tight w-[515px] !whitespace-break-spaces"
                       >
@@ -67,7 +72,9 @@ const Header = () => {
             <Slide direction="up" delay={1000} duration={1000} triggerOnce>
               <Fade duration={2000} delay={1000} triggerOnce>
                 <div className="custom-border backdrop-blur-[6px] px-12 pt-3 pb-5">
-                  <p className="font-bold text-white text-center mb-5">Time Left To Invest</p>
+                  <p className="font-bold text-white text-center mb-5">
+                    Time Left To Invest
+                  </p>
                   <div className="flex gap-4 items-center text-white">
                     <div className="box-filled rounded-md w-[50px] h-[50px] flex justify-center items-center text-[22px] text-white">
                       {days}
@@ -94,13 +101,20 @@ const Header = () => {
             <Fade duration={1500} triggerOnce>
               <div>
                 <div className="custom-border backdrop-blur-[6px] text-white py-6 px-5">
-                  <p className="text-[22px] font-bold text-center">Token Swap</p>
+                  <p className="text-[22px] font-bold text-center">
+                    Token Swap
+                  </p>
                   <div className="mb-4">
                     <p className="ml-4 mb-2.5">From</p>
                     <div className="bg-night flex justify-between items-center p-2 pr-6 rounded-full w-[324px]">
                       <div className="rounded-full bg-dark py-3 px-5 flex gap-6">
                         <div className="flex gap-2">
-                          <Image src={"/icon/eth-icon.svg"} alt="Ethereum" width={22} height={22} />
+                          <Image
+                            src={"/icon/eth-icon.svg"}
+                            alt="Ethereum"
+                            width={22}
+                            height={22}
+                          />
                           <b>USDT</b>
                         </div>
                         <Image
@@ -118,7 +132,12 @@ const Header = () => {
                     <div className="bg-night flex justify-between items-center p-2 pr-6 rounded-full w-[324px]">
                       <div className="rounded-full bg-dark py-3 px-5 flex gap-6">
                         <div className="flex gap-2">
-                          <Image src={"/icon/eth-icon.svg"} alt="Ethereum" width={22} height={22} />
+                          <Image
+                            src={"/icon/eth-icon.svg"}
+                            alt="Ethereum"
+                            width={22}
+                            height={22}
+                          />
                           <b>USDT</b>
                         </div>
                         <Image
@@ -134,8 +153,16 @@ const Header = () => {
                   <div className="py-6">
                     <Divider />
                   </div>
-                  <Button className="flex gap-4 justify-center items-center w-full" type={"fill"}>
-                    <Image src={"/icon/swap-icon.svg"} alt="Swap" width={16} height={16} />
+                  <Button
+                    className="flex gap-4 justify-center items-center w-full"
+                    type={"fill"}
+                  >
+                    <Image
+                      src={"/icon/swap-icon.svg"}
+                      alt="Swap"
+                      width={16}
+                      height={16}
+                    />
                     <span className="font-bold">Convert</span>
                   </Button>
                   <div className="flex justify-between items-center my-4 text-silver">
@@ -152,8 +179,9 @@ const Header = () => {
           <div className="w-[600px] text-center">
             <Fade delay={1e3} cascade damping={1e-1} triggerOnce>
               <p className="pb-5">
-                We are feeding the world, by providing a frictionless food supply chain marketplace
-                that brings together all parties in the agri-food value chain.
+                We are feeding the world, by providing a frictionless food
+                supply chain marketplace that brings together all parties in the
+                agri-food value chain.
               </p>
             </Fade>
             <Fade delay={1200} cascade damping={0.1} triggerOnce>
@@ -166,21 +194,39 @@ const Header = () => {
       <div className="bg-[url('/svg/bg-header-sm.svg')] w-full bg-cover h-screen min-h-[1150px] bg-no-repeat lg:hidden">
         <div className="section-margin-x">
           <div className="py-4 flex justify-between items-center">
-            <Image src={"/logo-sm.svg"} alt="Food sply" width={132} height={48} />
+            <Image
+              src={"/logo-sm.svg"}
+              alt="Food sply"
+              width={132}
+              height={48}
+            />
             <HamburgerIcon />
           </div>
           <div className="py-4">
-            <Image src={"/images/header-title-one.svg"} alt="Food sply" width={235} height={18} />
+            <Image
+              src={"/images/header-title-one.svg"}
+              alt="Food sply"
+              width={235}
+              height={18}
+            />
 
             <h1 className="text-border text-white font-bold text-5xl capitalize mt-4 leading-tight">
-              <Fade delay={1e3} cascade damping={1e-1} triggerOnce className="!whitespace-break-spaces">
+              <Fade
+                delay={1e3}
+                cascade
+                damping={1e-1}
+                triggerOnce
+                className="!whitespace-break-spaces"
+              >
                 AgriTech Industry Nourishing a Sustainable Future
               </Fade>
             </h1>
 
             {/* Timer */}
             <div className="custom-border backdrop-blur-[6px] px-6 pt-3 pb-5 mt-6">
-              <p className="font-bold text-white text-center mb-5">Time Left To Invest</p>
+              <p className="font-bold text-white text-center mb-5">
+                Time Left To Invest
+              </p>
 
               <div className="flex gap-4 items-baseline text-white">
                 <div className="flex flex-col items-center">
@@ -220,10 +266,20 @@ const Header = () => {
                 <div className="bg-night flex justify-between items-center p-2 pr-6 rounded-full w-[324px]">
                   <div className="rounded-full bg-dark py-3 px-5 flex gap-6">
                     <div className="flex gap-2">
-                      <Image src={"/icon/eth-icon.svg"} alt="Ethereum" width={22} height={22} />
+                      <Image
+                        src={"/icon/eth-icon.svg"}
+                        alt="Ethereum"
+                        width={22}
+                        height={22}
+                      />
                       <b>USDT</b>
                     </div>
-                    <Image src={"/icon/down-arrow.svg"} alt="Arrow Down" width={13} height={22} />
+                    <Image
+                      src={"/icon/down-arrow.svg"}
+                      alt="Arrow Down"
+                      width={13}
+                      height={22}
+                    />
                   </div>
                   <p className="text-text-grey text-sm">Balance: 430.00</p>
                 </div>
@@ -233,10 +289,20 @@ const Header = () => {
                 <div className="bg-night flex justify-between items-center p-2 pr-6 rounded-full w-[324px]">
                   <div className="rounded-full bg-dark py-3 px-5 flex gap-6">
                     <div className="flex gap-2">
-                      <Image src={"/icon/eth-icon.svg"} alt="Ethereum" width={22} height={22} />
+                      <Image
+                        src={"/icon/eth-icon.svg"}
+                        alt="Ethereum"
+                        width={22}
+                        height={22}
+                      />
                       <b>USDT</b>
                     </div>
-                    <Image src={"/icon/down-arrow.svg"} alt="Arrow Down" width={13} height={22} />
+                    <Image
+                      src={"/icon/down-arrow.svg"}
+                      alt="Arrow Down"
+                      width={13}
+                      height={22}
+                    />
                   </div>
                   <p className="text-text-grey text-sm">Balance: 430.00</p>
                 </div>
@@ -244,8 +310,16 @@ const Header = () => {
               <div className="py-6">
                 <Divider />
               </div>
-              <Button className="flex gap-4 justify-center items-center w-full" type={"fill"}>
-                <Image src={"/icon/swap-icon.svg"} alt="Swap" width={16} height={16} />
+              <Button
+                className="flex gap-4 justify-center items-center w-full"
+                type={"fill"}
+              >
+                <Image
+                  src={"/icon/swap-icon.svg"}
+                  alt="Swap"
+                  width={16}
+                  height={16}
+                />
                 <span className="font-bold">Convert</span>
               </Button>
               <div className="flex justify-between items-center my-4 text-silver">

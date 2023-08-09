2bd53af2f6407a34f7d640be13ff560532112722
Ali <raza8r@gmail.com>
2023-08-09 21:59:09 +0500

    adjust mobileapp for sm screens

--- src/components/MobileApp.jsx
+++ src/components/MobileApp.jsx
@@ -4,22 +4,16 @@ import React from "react";
 const MobileApp = () => {
   return (
     <div className="background-mobile">
-      <div className="section-margin-x py-[115px] flex justify-center items-center lg:gap-48 xl:gap-64">
+      <div className="section-margin-x py-12 lg:py-[115px] flex justify-center items-center lg:gap-48 xl:gap-64">
         {/* First col */}
         <div className="">
-          <Image
-            src="/logo2.svg"
-            alt="food suply"
-            width={315}
-            height={94}
-            className="mb-8"
-          />
-          <p className="text-text-dark text-xl mb-[88px] w-[526px]">
+          <Image src="/logo2.svg" alt="food suply" width={315} height={94} className="mb-8" />
+          <p className="hidden md:block text-text-dark text-xl mb-[88px] w-[526px]">
             Discover the freshest agricultural products with FOODSPLY, now on mobile! Download now
             on Google Play or the Apple Store and connect directly with the farm-to-table
             experience.
           </p>
-          <div className="flex items-center gap-6">
+          <div className="flex flex-col lg:flex-row items-center gap-6">
             <Image
               src={"/svg/mobile-app/googleplay.svg"}
               alt="Food sply app"

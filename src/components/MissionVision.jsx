ccd0b4fc1c2c04dcfd5d738db81444f87018146e
Ali <raza8r@gmail.com>
2023-08-10 18:49:59 +0500

    add animation for mission section

--- src/components/MissionVision.jsx
+++ src/components/MissionVision.jsx
@@ -1,36 +1,43 @@
+"use client";
 import Image from "next/image";
 import React from "react";
+import { Fade, Slide } from "react-awesome-reveal";
 
 const MissionVision = () => {
   return (
-    <div className="section-margin !my-[70px] lg:my-auto flex flex-col-reverse lg:flex-row justify-between items-center lg:gap-40">
-      {/* First col */}
-      <div className="w-[70%] lg:w-auto mt-10 lg:mt-auto">
-        <Image
-          src={"/svg/mission-vision/mission-img.svg"}
-          alt="Food sply mission and vision"
-          width={350}
-          height={550}
-        />
-      </div>
-      {/* Second col */}
-      <div className="lg:w-[730px]">
-        <div className="mb-8 lg:mb-[52px]">
-          <h2 className="text-2xl lg:text-5xl text-green mb-3 lg:mb-6 font-bold">Mission</h2>
-          <p className="text-base lg:text-[22px] text-text-dark">
-            We are feeding the future by connecting farmers, suppliers, and consumers through our
-            FOODSPLY Platform, building a network that nourishes today and sustains tomorrow.
-          </p>
+    <Slide direction="up" fraction={0} triggerOnce>
+      <Fade triggerOnce delay={200} duration={2000}>
+        <div className="section-margin !my-[70px] lg:my-auto flex flex-col-reverse lg:flex-row justify-between items-center lg:gap-40">
+          {/* First col */}
+          <div className="w-[70%] lg:w-auto mt-10 lg:mt-auto">
+            <Image
+              src={"/svg/mission-vision/mission-img.svg"}
+              alt="Food sply mission and vision"
+              width={350}
+              height={550}
+            />
+          </div>
+          {/* Second col */}
+          <div className="lg:w-[730px]">
+            <div className="mb-8 lg:mb-[52px]">
+              <h2 className="text-2xl lg:text-5xl text-green mb-3 lg:mb-6 font-bold">Mission</h2>
+              <p className="text-base lg:text-[22px] text-text-dark">
+                We are feeding the future by connecting farmers, suppliers, and consumers through
+                our FOODSPLY Platform, building a network that nourishes today and sustains
+                tomorrow.
+              </p>
+            </div>
+            <div className="">
+              <h2 className="text-2xl lg:text-5xl text-green mb-3 lg:mb-6 font-bold">Our Vision</h2>
+              <p className="text-base lg:text-[22px] text-text-dark">
+                Our vision is to create a food supply chain marketplace that brings together all
+                parties in the agri-food value chain.
+              </p>
+            </div>
+          </div>
         </div>
-        <div className="">
-          <h2 className="text-2xl lg:text-5xl text-green mb-3 lg:mb-6 font-bold">Our Vision</h2>
-          <p className="text-base lg:text-[22px] text-text-dark">
-            Our vision is to create a food supply chain marketplace that brings together all parties
-            in the agri-food value chain.
-          </p>
-        </div>
-      </div>
-    </div>
+      </Fade>
+    </Slide>
   );
 };
 
--- src/components/MobileApp.jsx
+++ src/components/MobileApp.jsx
@@ -1,37 +1,49 @@
+"use client";
 import Image from "next/image";
 import React from "react";
+import { Fade, Slide, Bounce } from "react-awesome-reveal";
 
 const MobileApp = () => {
   return (
     <div className="background-mobile">
-      <div className="section-margin-x py-12 lg:py-[115px] flex justify-center items-center lg:gap-48 xl:gap-64">
+      <div className="section-margin-x py-12 lg:py-[115px] flex justify-center items-center gap-8 lg:gap-48 xl:gap-64">
         {/* First col */}
         <div className="">
-          <Image src="/logo2.svg" alt="food suply" width={315} height={94} className="mb-8" />
-          <p className="hidden md:block text-text-dark text-xl mb-[88px] w-[526px]">
-            Discover the freshest agricultural products with FOODSPLY, now on mobile! Download now
-            on Google Play or the Apple Store and connect directly with the farm-to-table
-            experience.
-          </p>
+          <Slide direction="top" duration={1000} triggerOnce>
+            <Image src="/logo2.svg" alt="food suply" width={315} height={94} className="mb-8" />
+          </Slide>
+          <Fade duration={1500} delay={600} triggerOnce>
+            <p className="hidden md:block text-text-dark text-xl mb-[88px] w-[526px]">
+              Discover the freshest agricultural products with FOODSPLY, now on mobile! Download now
+              on Google Play or the Apple Store and connect directly with the farm-to-table
+              experience.
+            </p>
+          </Fade>
           <div className="flex flex-col lg:flex-row items-center gap-6">
-            <Image
-              src={"/svg/mobile-app/googleplay.svg"}
-              alt="Food sply app"
-              width={200}
-              height={60}
-            />
-            <Image
-              src={"/svg/mobile-app/appstore.svg"}
-              alt="Food sply app"
-              width={200}
-              height={60}
-            />
+            <Bounce duration={1500} delay={600} triggerOnce>
+              <Image
+                src={"/svg/mobile-app/googleplay.svg"}
+                alt="Food sply app"
+                width={200}
+                height={60}
+                className="link cursor-pointer"
+              />
+              <Image
+                src={"/svg/mobile-app/appstore.svg"}
+                alt="Food sply app"
+                width={200}
+                height={60}
+                className="link cursor-pointer"
+              />
+            </Bounce>
           </div>
         </div>
         {/* Second col */}
-        <div>
-          <Image src="/svg/mobile-app/phone.svg" alt="food suply" width={305} height={615} />
-        </div>
+        <Slide direction="right" duration={1000} triggerOnce>
+          <div>
+            <Image src="/svg/mobile-app/phone.svg" alt="food suply" width={305} height={615} />
+          </div>
+        </Slide>
       </div>
     </div>
   );

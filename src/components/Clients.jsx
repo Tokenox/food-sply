99ca74505a920512374ea7e65df2e871f6aad3f7
Ali <raza8r@gmail.com>
2023-08-12 14:55:15 +0500

    fixed client section scroll

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -7,7 +7,7 @@ import { Fade, Slide } from "react-awesome-reveal";
 const Clients = () => {
   return (
     <div className="bg-bg-dark">
-      <div className="section-margin-x py-[40px] flex gap-6 2xl:gap-[75px] items-center justify-between overflow-x-scroll">
+      <div className="section-margin-x py-[40px] flex gap-6 2xl:gap-[75px] items-center justify-between overflow-x-auto">
         {ClientData?.map((item, i) => (
           <Slide key={i} triggerOnce direction="up" delay={i * 150}>
             <Fade triggerOnce delay={i * 200}>

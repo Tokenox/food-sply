aa4afca06ceb584b7ef153c10be60bb924d45ce4
Ali <raza8r@gmail.com>
2023-09-10 03:37:25 +0500

    design nft header section

--- src/components/Nft/NftHeader.jsx
+++ src/components/Nft/NftHeader.jsx
@@ -2,13 +2,15 @@ import Button from "@/common/Button";
 import Image from "next/image";
 import React from "react";
 import Navbar from "../Navbar";
+import ladyVector from "../../../public/nft-images/header/lady-vector.svg";
+import menVector from "../../../public/nft-images/header/men-vector.svg";
 
 const NftHeader = () => {
   return (
-    <div className="bg-[url('/nft-images/header/nft-header.svg')] h-screen w-full bg-no-repeat bg-cover bg-center min-h-[800px]">
+    <div className="bg-[url('/nft-images/header/nft-header.svg')] h-screen w-full bg-no-repeat bg-cover bg-center">
       <Navbar />
       <div className="section-margin-x">
-        <div className="flex justify-center mt-[80px]">
+        <div className="flex justify-center mt-[40px] min-[1400px]:mt-[80px]">
           <Image
             src="/nft-images/header/nft-header-text.svg"
             alt="discover and collect nfts"
@@ -16,40 +18,38 @@ const NftHeader = () => {
             height={63}
           />
         </div>
-        <div className="relative flex justify-center mt-8">
+        <div className="relative flex justify-center mt-7 min-[1400px]:mt-8">
           <div className="relative z-[1]">
             <Image
-              src="/nft-images/header/men-vector.svg"
+              src={menVector}
               alt="food sply nft"
-              width={302}
-              height={480}
+              className="w-[400px] lg:h-[390px] min-[1400px]:h-[570px]  object-contain"
             />
           </div>
-          <div className="w-[695px] flex flex-col items-center">
-            <h2 className="text-border-v2 text-[42px] text-white capitalize w-[630px]">
+          <div className="w-[595px] flex flex-col items-center">
+            <h2 className="text-border-v2 text-center text-[42px] text-white capitalize w-[630px]">
               Own Tomorrow&apos;s Harvest Today
             </h2>
-            <p className="text-2xl text-white capitalize text-center w-[544px] my-9">
+            <p className="text-2xl text-white capitalize text-center w-[544px] my-6 min-[1400px]:my-9">
               Dive into a digital harvest where each NFT stands for sustainability and tech harmony.
               Cultivate the future, one veggie at a time.
             </p>
-            <Button type="fill" className="text-xl font-bold py-3">
+            <Button type="fill" className="text-xl font-bold py-3 mt-4 min-[1400px]:mt-0">
               Mint My Veggie NFT
             </Button>
             <Image
               src="/nft-images/header/header-line.svg"
               alt="discover and collect nfts"
-              width={1050}
+              width={1070}
               height={200}
-              className="absolute bottom-[-60px]"
+              className="absolute hidden min-[1450px]:block bottom-[-50px]"
             />
           </div>
           <div className="relative z-[1]">
             <Image
-              src="/nft-images/header/lady-vector.svg"
+              src={ladyVector}
               alt="food sply nft"
-              width={270}
-              height={480}
+              className="w-[400px] lg:h-[390px] min-[1400px]:h-[570px] object-contain"
             />
           </div>
         </div>

cbadaa8ddeaffe10c4f635bd9e1a9277a4c09561
Ali <raza8r@gmail.com>
2023-09-16 06:35:48 +0500

    update nft backgroud image

--- src/components/Nft/NftHeader.jsx
+++ src/components/Nft/NftHeader.jsx
@@ -14,7 +14,7 @@ const NftHeader = () => {
 
   return (
     <Fragment>
-      <div className="hidden lg:block bg-[url('/nft-images/header/nft-header.svg')] h-screen w-full bg-no-repeat bg-cover bg-center">
+      <div className="hidden lg:block bg-[url('/nft-images/header/hero-section-lg.png')] h-screen min-h-[700px] w-full bg-no-repeat bg-cover bg-inherit 2xl:bg-center">
         <Navbar />
         <div className="section-margin-x">
           <div className="flex justify-center mt-[40px] min-[1400px]:mt-[80px]">
@@ -26,13 +26,13 @@ const NftHeader = () => {
             />
           </div>
           <div className="relative flex justify-center mt-7 min-[1400px]:mt-8">
-            <div className="relative z-[1]">
+            {/* <div className="relative z-[1]">
               <Image
                 src={menVector}
                 alt="food sply nft"
                 className="w-[400px] lg:h-[390px] min-[1400px]:h-[570px]  object-contain"
               />
-            </div>
+            </div> */}
             <div className="w-[595px] flex flex-col items-center">
               <h2 className="text-border-v2 text-center text-[42px] font-medium text-white capitalize w-[630px]">
                 Own Tomorrow&apos;s Harvest Today
@@ -44,21 +44,21 @@ const NftHeader = () => {
               <Button type="fill" className="text-xl font-bold py-3 mt-4 min-[1400px]:mt-3">
                 Mint My Veggie NFT
               </Button>
-              <Image
+              {/* <Image
                 src="/nft-images/header/header-line.svg"
                 alt="discover and collect nfts"
                 width={1070}
                 height={200}
                 className="absolute hidden min-[1400px]:block bottom-[-30px] 2xl:bottom-[-56px]"
-              />
+              /> */}
             </div>
-            <div className="relative z-[1]">
+            {/* <div className="relative z-[1]">
               <Image
                 src={ladyVector}
                 alt="food sply nft"
                 className="w-[400px] lg:h-[390px] min-[1400px]:h-[570px] object-contain"
               />
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
